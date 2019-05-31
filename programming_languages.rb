d69cdec782516703d04854d2e6f8e1890407c204
Lindsey Wells <lindsey.wells84@gmail.com>
2019-05-31 15:50:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -39,7 +39,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |oo_or_f, language_hash|
     language_hash.each do |name, type_hash|
-      new_hash[name]=type_hash
+      new_hash[name] ||= type_hash
       new_hash[name][:style] ||= []
       new_hash[name][:style] << oo_or_f
 
